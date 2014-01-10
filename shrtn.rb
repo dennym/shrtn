43c9569ef8ea17aeb0f55203ab33202b5b65d27c
Denny Mueller <denny@dennymueller.de>
2014-01-10 16:47:19 +0100

    reworking redis stuff, fix intends, bugfixes

--- shrtn.rb
+++ shrtn.rb
@@ -1,34 +1,43 @@
 require 'sinatra'
 require 'redis'
 
+configure do
+
+	SiteConfig = OpenStruct.new(
+					:title => 'shrtn » url shortener',
+					:author => 'Denny Mueller',
+					:url_base => 'http://localhost:4567/' # the url of your application
+				)
+end
+
 redis = Redis.new
 
 helpers do
-  include Rack::Utils
-  alias_method :h, :escape_html
+	include Rack::Utils
+	alias_method :h, :escape_html
 
-  def random_string(length)
-    rand(36**length).to_s(36)
-  end
-
-  def get_site_url(short_url)
-		SiteConfig.url_base + short_url
+	def random_string(length)
+		rand(36**length).to_s(36)
 	end
+
+	def get_site_url(short_url)
+		SiteConfig.url_base + short_url
+		end
 end
 
 get '/' do
-  erb :index
+	erb :index
 end
 
 post '/' do
-  if params[:url] and not params[:url].empty?
-    @shortcode = random_string 5
-    redis.multi do
-    	redis.set "links:#{@shortcode}", params[:url], :nx => true, :ex => 20 
-  		redis.set "clicks:#{@shortcode}", "0", :nx => true, :ex => 20 
-  	end
-  end
-  erb :index
+	if params[:url] and not params[:url].empty?
+		@shortcode = random_string 5
+		redis.multi do
+			redis.set "links:#{@shortcode}", params[:url], :nx => true, :ex => 400
+			redis.set "clicks:#{@shortcode}", "0", :nx => true, :ex => 400
+		end
+	end
+	erb :index
 end
 
 get '/admin' do
@@ -37,10 +46,9 @@ get '/admin' do
 end
 
 get '/:shortcode' do
-	redis.multi do
-  	redis.incr "clicks:#{params[:shortcode]}"
-  	@url = redis.get "links:#{params[:shortcode]}"
-  end
-  redirect @url || '/'
+	redis.pipelined do
+		redis.incr "clicks:#{params[:shortcode]}"
+		@url = redis.get "links:#{params[:shortcode]}"
+	end
+	redirect @url.value || '/'
 end
-
